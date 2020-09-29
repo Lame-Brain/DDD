083096b62bdae03b1731f978ce33cc40c6dd16d3
Ethan Roberts <theLameBrain@gmail.com>
2020-09-29 13:16:38 -0700

    First Commit

---
+++ Assets/IntroMenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroMenuController : MonoBehaviour
+{
+    public Light fireLight;
+    public float FlickerSpeed;
+    private float flPosX, flPosY, flIntensity, counter = 0;
+
+    public AudioSource TEST;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter > FlickerSpeed)
+        {
+            counter = 0;
+            flPosX = Random.Range(-1f, 1f); flPosY = Random.Range(0f, 10f); flIntensity = Random.Range(1f, 3f);
+            //fireLight.transform.position = new Vector3(flPosX, flPosY, 6.5f);
+            fireLight.intensity = flIntensity;
+        }
+    }
+}
