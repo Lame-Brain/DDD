40ec34d5a685a886c5fc8f62ec6d520334e84c0a
Ethan Roberts <theLameBrain@gmail.com>
2020-10-03 23:25:32 -0700

    Implemented Calendar

--- Assets/Scripts/ThelmoreManager.cs
+++ Assets/Scripts/ThelmoreManager.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class ThelmoreManager : MonoBehaviour
 {
-    public Text InfoText, TimeText, DayText, MonthText;
-    private int selectedBuilding, time = 0, day = 0, month = 0;
+    public Text InfoText, TimeText;
+    private int selectedBuilding, time = 0, day = 1, month = 0;
        
 
     // Start is called before the first frame update
@@ -20,15 +20,123 @@ public class ThelmoreManager : MonoBehaviour
     void Update()
     {
         //Display Time, Day, and Month correctly.
-        if (time == 0) TimeText.text = "<color=blue>Early Morning</color>";
-        if (time > 0 && time < 3) TimeText.text = "<color=yellow>Morning</color>";
-        if (time > 2 && time < 5) TimeText.text = "<color=blue>Noon</color>";
-        if (time > 4 && time < 7) TimeText.text = "<color=cyan>Afternoon</color>";
-        if (time == 7) TimeText.text = "<color=magenta>Sunset</color>";
-        if (time > 7 && time < 10) TimeText.text = "<color=teal>Early Night</color>";
-        if (time > 9 && time < 12) TimeText.text = "<color=black>Midnight</color>";
-        if (time > 11 && time < 15) TimeText.text = "<color=darkblue>Late Night</color>";
+        string timeOutput = "";
+        if (time > 11) { time = 0; day++; }
+        if (time == 0) timeOutput = "<color=blue>Early Morning</color> ";
+        if (time > 0 && time < 3) TimeText.text = "<color=yellow>Morning</color> ";
+        if (time == 3) timeOutput = "<color=blue>Noon</color> ";
+        if (time > 4 && time < 6) timeOutput = "<color=cyan>Afternoon</color> ";
+        if (time == 6) TimeText.text = "<color=magenta>Sunset</color> ";
+        if (time > 6 && time < 9) timeOutput = "<color=teal>Early Night</color> ";
+        if (time == 9) TimeText.text = "<color=black>Midnight</color> ";
+        if (time > 9 && time < 12) timeOutput = "<color=darkblue>Late Night</color> ";
 
-        if()
+        string stringOutput = "";
+        if (month == 0)
+        {
+            stringOutput += "";
+            if (day > 1) AdvanceMonth();
+        }
+        if (month == 1)
+        {
+            stringOutput += "<color=magenta>Holy to GREAT MOTHER</color>";
+            if (day > 4) AdvanceMonth();
+        }
+        if (month == 2)
+        {
+            stringOutput += "of the <color=white>Month of UL</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 3)
+        {
+            stringOutput += "of the <color=white>Month of NIOTL</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 4)
+        {
+            stringOutput += "of the <color=white>Month of OZTOZ</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 5)
+        {
+            stringOutput += "of <color=magenta>the Spring Equinox</color>";
+            if (day > 7) AdvanceMonth();
+        }
+        if (month == 6)
+        {
+            stringOutput += "of the <color=lime>Month of FOTA</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 7)
+        {
+            stringOutput += "of the <color=lime>Month of ADAR</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 8)
+        {
+            stringOutput += "of the <color=lime>Month of VIUNA</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 9)
+        {
+            stringOutput += "of <color=magenta>the Summer Solstice</color>";
+            if (day > 6) AdvanceMonth();
+        }
+        if (month == 10)
+        {
+            stringOutput += "of the <color=yellow>Month of IAMUS</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 11)
+        {
+            stringOutput += "of the <color=yellow>Month of SAMDIOME</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 12)
+        {
+            stringOutput += "of the <color=yellow>Month of ELIUS</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 13)
+        {
+            stringOutput += "of <color=magenta>the Autumn Equinox</color>";
+            if (day > 7) AdvanceMonth();
+        }
+        if (month == 14)
+        {
+            stringOutput += "of the <color=brown>Month ZEDIA</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 15)
+        {
+            stringOutput += "of the <color=brown>Month of YNARUS</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 16)
+        {
+            stringOutput += "of the <color=brown>Month of RANERA</color>";
+            if (day > 28) AdvanceMonth();
+        }
+        if (month == 17)
+        {
+            stringOutput += "<color=magenta>Holy to the WISE CRONE</color>";
+            if (day > 4) AdvanceMonth();
+        }
+        string dayOutput = "the " + day + "th day ";
+        if (day == 1) dayOutput = "the 1st day ";
+        if (day == 2) dayOutput = "the 2nd day ";
+        if (day == 3) dayOutput = "the 3rd day ";
+        if (day == 21) dayOutput = "the 21st day ";
+        if (day == 22) dayOutput = "the 22nd day ";
+        if (day == 23) dayOutput = "the 23rd day ";
+        if(month == 0) dayOutput = "of the <color=white>THE WINTER SOLSTICE</color> ";
+        TimeText.text = timeOutput + dayOutput + stringOutput;
+    }
+
+    public void AdvanceMonth()
+    {
+        day = 1;
+        month++;
+        if (month > 17) month = 0;
     }
 }
